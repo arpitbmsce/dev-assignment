87f21ed6d1bba0ed53879616e27a7f21107e760b
arpit saxena <arpitbmsce@users.noreply.github.com>
2019-09-14 11:43:21 +0530

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,42 @@
+// Code goes here
+
+let existingArray = [7000, 7001,7002, 7003, 7004, 7005]
+
+
+function validateListOfNumbers () {
+  
+  let data = String(document.getElementById("listOfNumbers").value);
+  
+  if(data === "") {
+    return;
+  }
+  data = data.split(',');
+  let finalList = [...existingArray], additionalList = [], duplicateList = "";
+  let updateArrayAndDuplicateList = function(elem) {
+    if(isNaN(elem)) {
+      return;
+    }
+    if (existingArray.indexOf(elem) === -1 )         {
+        additionalList.push(elem);
+       } else {
+        duplicateList = (duplicateList !== "" ? (duplicateList + ', ' ) : duplicateList) + elem 
+       }
+  }
+  data.forEach(function(elem) {
+    let indexOfHyphen = elem.indexOf('-');
+    if (indexOfHyphen > -1) {
+      // alert(900);
+      let startNum = parseInt(elem.substring(0, indexOfHyphen).trim());
+      let endNum = parseInt(elem.substring(indexOfHyphen + 1, elem.length).trim());
+
+      for(let i = startNum; i<= endNum; i++) {
+        updateArrayAndDuplicateList(i);
+      }
+    } else {
+    updateArrayAndDuplicateList(parseInt(elem.trim()));
+    }
+  });
+    finalList.push(...additionalList);
+    alert(finalList);
+    alert(duplicateList);
+ }
